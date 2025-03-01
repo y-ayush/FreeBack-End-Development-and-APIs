6b40f79da167f45c3c3c53f481f4d7b63a75709b
y-ayush <ayush1212yadav@gmail.com>
2025-03-01 21:36:59 +0530

    minnor

--- timestampe/index.js
+++ timestampe/index.js
@@ -1,3 +1,8 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(3000, () => console.log("Listening on port 3000"));
 app.get('/api/:date?', (req, res) => {
     let { date } = req.params;
     console.log("Raw input:", date);
